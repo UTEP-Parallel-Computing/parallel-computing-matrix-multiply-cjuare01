dcb916ae431b6c7feef4d74d351430c5a3f50bad
Charlie Juarez <cjuare01@yahoo.com>
2020-02-02 10:04:30 -0700

    Fixed multiplication of matrices

--- matrix.py
+++ matrix.py
@@ -12,6 +12,7 @@ class Matrix(object):
         self.column = column
         self.matrix = matrix
 
+    # Function to print the values inside the matrix
     def print_matrix(self):
         for num1 in range(self.row):
             for num2 in range(self.column):
@@ -26,38 +27,48 @@ def main():
     matrix3 = multiply_matrices(matrix1, matrix2)
     end = time.time()
     elapsed_time = end - start
-    print(elapsed_time)
+    print('The multiplication of the matrices took '+str(elapsed_time)+' seconds.')
 
-
+# Function to create a matrix
 def create_matrix():
     matrix = []
     row = int(input('Provide the number of rows in the matrix: '))
     column = int(input('Provide the number of columns in the matrix: '))
+
+    # Input the values into the matrix
     for num1 in range(row):
         temp_arr = []
         for num2 in range(column):
             temp_arr.append(int(input('Provide a number to input into the matrix: ')))
         matrix.append(temp_arr)
+
     matrix = Matrix(row, column, matrix)
     print('With your response you have created the following matrix:')
     matrix.print_matrix()
     return matrix
 
-
+# Function to multiply matrices
 def multiply_matrices(matrix1, matrix2):
-    matrix3 = []
-    row = matrix1.row
-    column = matrix2.column
-    for num1 in range(row):
+    matrix = []
+
+    # Create an empty matrix with the appropriate dimensions
+    for num1 in range(matrix1.row):
         temp_arr = []
-        result = 0
-        for num2 in range(column):
-            temp1 = matrix1.matrix[num1][num2]
-            temp2 = matrix2.matrix[num2][num1]
-            result += temp1*temp2
-            temp_arr.append(result)
-        matrix3.append(temp_arr)
-    matrix3 = Matrix(row, column, matrix3)
+        for num2 in range(matrix2.column):
+            temp_arr.append(0)
+        matrix.append(temp_arr)
+
+    # Fill in the values of the product of matrices
+    # Iterate through rows of matrix1
+    for i in range(matrix1.row):
+        # Iterate through columns of matrix2
+        for j in range(matrix2.column):
+            # Iterate through rows of matrix2
+            for k in range(matrix2.row):
+                matrix[i][j] += matrix1.matrix[i][k] * matrix2.matrix[k][j]
+    
+    matrix3 = Matrix(matrix1.row, matrix2.column, matrix)
+    print('The multiplication of the matrices have yielded the following matrix:')
     matrix3.print_matrix()
     return matrix3
 
